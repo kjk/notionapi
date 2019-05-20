23725c8624155e704cd174547cab092c1906a0fb
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-05-20 16:17:17 -0700

    add HTMLRenderer.AddHeaderAnchor option

--- tohtml/html.go
+++ tohtml/html.go
@@ -31,6 +31,10 @@ type HTMLRenderer struct {
 	// if true, adds id=${NotionID} attribute to HTML nodes
 	AddIDAttribute bool
 
+	// if true, adds <a href="#{$NotionID}">svg(anchor-icon)</a>
+	// to h1/h2/h3
+	AddHeaderAnchor bool
+
 	// allows over-riding rendering of specific blocks
 	// return false for default rendering
 	RenderBlockOverride BlockRenderFunc
@@ -155,8 +159,9 @@ func (r *HTMLRenderer) WriteElement(block *notionapi.Block, tag string, attrs []
 		r.WriteIndent()
 		r.WriteString(content)
 		r.Newline()
+	} else {
+		r.RenderInlines(block.InlineContent)
 	}
-	r.RenderInlines(block.InlineContent)
 	r.Newline()
 }
 
@@ -391,7 +396,12 @@ func (r *HTMLRenderer) RenderHeaderLevel(block *notionapi.Block, level int, ente
 	el := fmt.Sprintf("h%d", level)
 	cls := fmt.Sprintf("notion-header-%d", level)
 	attrs := []string{"class", cls}
-	r.WriteElement(block, el, attrs, "", entering)
+	content := r.GetInlineContent(block.InlineContent)
+	id := r.maybeGetID(block)
+	if r.AddHeaderAnchor {
+		content += fmt.Sprintf(` <a class="notion-header-anchor" href="#%s" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8"><path d="M5.88.03c-.18.01-.36.03-.53.09-.27.1-.53.25-.75.47a.5.5 0 1 0 .69.69c.11-.11.24-.17.38-.22.35-.12.78-.07 1.06.22.39.39.39 1.04 0 1.44l-1.5 1.5c-.44.44-.8.48-1.06.47-.26-.01-.41-.13-.41-.13a.5.5 0 1 0-.5.88s.34.22.84.25c.5.03 1.2-.16 1.81-.78l1.5-1.5c.78-.78.78-2.04 0-2.81-.28-.28-.61-.45-.97-.53-.18-.04-.38-.04-.56-.03zm-2 2.31c-.5-.02-1.19.15-1.78.75l-1.5 1.5c-.78.78-.78 2.04 0 2.81.56.56 1.36.72 2.06.47.27-.1.53-.25.75-.47a.5.5 0 1 0-.69-.69c-.11.11-.24.17-.38.22-.35.12-.78.07-1.06-.22-.39-.39-.39-1.04 0-1.44l1.5-1.5c.4-.4.75-.45 1.03-.44.28.01.47.09.47.09a.5.5 0 1 0 .44-.88s-.34-.2-.84-.22z"></path></svg></a>`, id)
+	}
+	r.WriteElement(block, el, attrs, content, entering)
 	return true
 }
 
