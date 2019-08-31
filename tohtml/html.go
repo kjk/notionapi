0dfe517b50d6484f850c3346357523cac52b2fab
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-08-30 22:08:34 -0700

    forgot to render inlines

--- tohtml/html.go
+++ tohtml/html.go
@@ -332,6 +332,7 @@ func (c *Converter) RenderPage(block *notionapi.Block) {
 // RenderText renders BlockText
 func (c *Converter) RenderText(block *notionapi.Block) {
 	c.Printf(`<div class="notion-text">`)
+	c.RenderInlines(block.InlineContent)
 	c.RenderChildren(block)
 	c.Printf(`</div>`)
 }
@@ -339,6 +340,7 @@ func (c *Converter) RenderText(block *notionapi.Block) {
 // RenderEquation renders BlockEquation
 func (c *Converter) RenderEquation(block *notionapi.Block) {
 	c.Printf(`<div class="notion-equation">`)
+	c.RenderInlines(block.InlineContent)
 	c.RenderChildren(block)
 	c.Printf(`</div>`)
 }
@@ -351,6 +353,7 @@ func (c *Converter) RenderNumberedList(block *notionapi.Block) {
 	}
 	{
 		c.Printf(`<li class="notion-numbered-list">`)
+		c.RenderInlines(block.InlineContent)
 		c.RenderChildren(block)
 		c.Printf(`</li>`)
 	}
@@ -372,6 +375,7 @@ func (c *Converter) RenderBulletedList(block *notionapi.Block) {
 
 	{
 		c.Printf(`<li class="notion-bulleted-list">`)
+		c.RenderInlines(block.InlineContent)
 		c.RenderChildren(block)
 		c.Printf(`</li>`)
 	}
@@ -387,7 +391,7 @@ func (c *Converter) RenderBulletedList(block *notionapi.Block) {
 // RenderHeaderLevel renders BlockHeader, SubHeader and SubSubHeader
 func (c *Converter) RenderHeaderLevel(block *notionapi.Block, level int) {
 	c.Printf(`<h%d class="notion-header-%d">`, level, level)
-	c.Printf(c.GetInlineContent(block.InlineContent))
+	c.RenderInlines(block.InlineContent)
 	id := c.maybeGetID(block)
 	if c.AddHeaderAnchor {
 		c.Printf(` <a class="notion-header-anchor" href="#%s" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8"><path d="M5.88.03c-.18.01-.36.03-.53.09-.27.1-.53.25-.75.47a.5.5 0 1 0 .69.69c.11-.11.24-.17.38-.22.35-.12.78-.07 1.06.22.39.39.39 1.04 0 1.44l-1.5 1.5c-.44.44-.8.48-1.06.47-.26-.01-.41-.13-.41-.13a.5.5 0 1 0-.5.88s.34.22.84.25c.5.03 1.2-.16 1.81-.78l1.5-1.5c.78-.78.78-2.04 0-2.81-.28-.28-.61-.45-.97-.53-.18-.04-.38-.04-.56-.03zm-2 2.31c-.5-.02-1.19.15-1.78.75l-1.5 1.5c-.78.78-.78 2.04 0 2.81.56.56 1.36.72 2.06.47.27-.1.53-.25.75-.47a.5.5 0 1 0-.69-.69c-.11.11-.24.17-.38.22-.35.12-.78.07-1.06-.22-.39-.39-.39-1.04 0-1.44l1.5-1.5c.4-.4.75-.45 1.03-.44.28.01.47.09.47.09a.5.5 0 1 0 .44-.88s-.34-.2-.84-.22z"></path></svg></a>`, id)
@@ -417,6 +421,7 @@ func (c *Converter) RenderTodo(block *notionapi.Block) {
 		cls = "notion-todo-checked"
 	}
 	c.Printf(`<div class="%s">`, cls)
+	c.RenderInlines(block.InlineContent)
 	c.RenderChildren(block)
 	c.Printf(`</div>`)
 }
@@ -443,6 +448,7 @@ func (c *Converter) RenderToggle(block *notionapi.Block) {
 // RenderQuote renders BlockQuote
 func (c *Converter) RenderQuote(block *notionapi.Block) {
 	c.Printf(`<quote class="notion-quote">`)
+	c.RenderInlines(block.InlineContent)
 	c.RenderChildren(block)
 	c.Printf(`</quote>`)
 }
@@ -450,6 +456,7 @@ func (c *Converter) RenderQuote(block *notionapi.Block) {
 // RenderCallout renders BlockCallout
 func (c *Converter) RenderCallout(block *notionapi.Block) {
 	c.Printf(`<div class="notion-callout">`)
+	c.RenderInlines(block.InlineContent)
 	c.RenderChildren(block)
 	c.Printf(`</div>`)
 }
@@ -474,7 +481,7 @@ func (c *Converter) RenderBookmark(block *notionapi.Block) {
 	c.Printf(`</div>`)
 }
 
-// RenderVideo renders BlockTweet
+// RenderVideo renders BlockVideo
 func (c *Converter) RenderVideo(block *notionapi.Block) {
 	f := block.FormatVideo()
 	ws := fmt.Sprintf("%d", f.BlockWidth)
