55a8546aadddb28e666afa9683f1e343dc22e948
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-10-06 23:15:15 -0700

    improve do -wc

--- do/wc.go
+++ do/wc.go
@@ -3,22 +3,34 @@ package main
 import (
 	"fmt"
 	"path/filepath"
-	"strings"
 
 	"github.com/kjk/wc"
 )
 
-func notDataDir(name string) bool {
-	name = filepath.ToSlash(name)
-	return !strings.Contains(name, "data/")
+func excludeDirs(path string) bool {
+	path = filepath.Dir(path)
+	for len(path) > 0 {
+		if path == "." {
+			return true
+		}
+		name := filepath.Base(path)
+		if name == "node_modules" {
+			return false
+		}
+		if name == "tmpdata" {
+			return false
+		}
+		path = filepath.Dir(path)
+	}
+	return true
 }
 
-var srcFiles = wc.MakeAllowedFileFilterForExts(".go")
-var allFiles = wc.MakeFilterAnd(srcFiles, notDataDir)
+var srcFiles = wc.MakeAllowedFileFilterForExts(".go", ".js", ".html", ".css")
+var allFiles = wc.MakeFilterAnd(srcFiles, excludeDirs)
 
 func doLineCount() int {
 	stats := wc.NewLineStats()
-	err := stats.CalcInDir(".", srcFiles, true)
+	err := stats.CalcInDir(".", allFiles, true)
 	if err != nil {
 		fmt.Printf("doLineCount: stats.wcInDir() failed with '%s'\n", err)
 		return 1
