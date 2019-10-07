a53a625a09727f5c70c0c7c9818cb67138eb3861
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-10-07 02:22:23 -0700

    move excludeDirs code to github.com/kjk/wc

--- do/wc.go
+++ do/wc.go
@@ -2,30 +2,12 @@ package main
 
 import (
 	"fmt"
-	"path/filepath"
 
 	"github.com/kjk/wc"
 )
 
-func excludeDirs(path string) bool {
-	path = filepath.Dir(path)
-	for len(path) > 0 {
-		if path == "." {
-			return true
-		}
-		name := filepath.Base(path)
-		if name == "node_modules" {
-			return false
-		}
-		if name == "tmpdata" {
-			return false
-		}
-		path = filepath.Dir(path)
-	}
-	return true
-}
-
 var srcFiles = wc.MakeAllowedFileFilterForExts(".go", ".js", ".html", ".css")
+var excludeDirs = wc.MakeExcludeDirsFilter("node_modules", "tmpdata")
 var allFiles = wc.MakeFilterAnd(srcFiles, excludeDirs)
 
 func doLineCount() int {
