50b42a7e3423d04861dd5c774208ec66c4e6874f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-10-07 22:31:14 -0700

    update do util.go

--- do/util.go
+++ do/util.go
@@ -78,20 +78,10 @@ func openBrowser(url string) {
 	must(err)
 }
 
-func readZipFile(path string) map[string][]byte {
-	r, err := zip.OpenReader(path)
+func getHomeDir() string {
+	s, err := os.UserHomeDir()
 	must(err)
-	defer fileClose(r)
-	res := map[string][]byte{}
-	for _, f := range r.File {
-		rc, err := f.Open()
-		must(err)
-		d, err := ioutil.ReadAll(rc)
-		must(err)
-		_ = rc.Close()
-		res[f.Name] = d
-	}
-	return res
+	return s
 }
 
 // absolute path of the current directory
@@ -162,6 +152,32 @@ func writeFile(path string, data []byte) {
 	must(err)
 }
 
+func ls(path string) {
+	fi, err := os.Stat(path)
+	must(err)
+	fmt.Printf("%s %d\n", path, fi.Size())
+}
+
+func cpFile(dstPath, srcPath string) {
+	d, err := ioutil.ReadFile(srcPath)
+	must(err)
+	err = ioutil.WriteFile(dstPath, d, 0666)
+	must(err)
+}
+
+func removeFile(path string) {
+	err := os.Remove(path)
+	if err == nil {
+		logf("removeFile('%s')", path)
+		return
+	}
+	if os.IsNotExist(err) {
+		// TODO: maybe should print note
+		return
+	}
+	fmt.Printf("os.Remove('%s') failed with '%s'\n", path, err)
+}
+
 func readFile(path string) []byte {
 	d, err := ioutil.ReadFile(path)
 	must(err)
@@ -195,7 +211,7 @@ func openCodeDiff(path1, path2 string) {
 	must(err)
 }
 
-func runCmd(cmd *exec.Cmd) {
+func runCmd(cmd *exec.Cmd) string {
 	fmt.Printf("> %s\n", cmd)
 	canCapture := (cmd.Stdout == nil) && (cmd.Stderr == nil)
 	if canCapture {
@@ -204,15 +220,121 @@ func runCmd(cmd *exec.Cmd) {
 			if len(out) > 0 {
 				fmt.Printf("Output:\n%s\n", string(out))
 			}
-			return
+			return string(out)
 		}
 		fmt.Printf("cmd '%s' failed with '%s'. Output:\n%s\n", cmd, err, string(out))
 		must(err)
+		return string(out)
 	}
 	err := cmd.Run()
 	if err == nil {
-		return
+		return ""
 	}
 	fmt.Printf("cmd '%s' failed with '%s'\n", cmd, err)
+	must(err)
+	return ""
+}
+
+func gitPull(dir string) {
+	cmd := exec.Command("git", "pull")
+	if dir != "" {
+		cmd.Dir = dir
+	}
+	runCmd(cmd)
+}
+
+func gitStatus(dir string) string {
+	cmd := exec.Command("git", "status")
+	if dir != "" {
+		cmd.Dir = dir
+	}
+	return runCmd(cmd)
+}
+
+func checkGitClean(dir string) {
+	s := gitStatus(dir)
+	expected := []string{
+		"On branch master",
+		"Your branch is up to date with 'origin/master'.",
+		"nothing to commit, working tree clean",
+	}
+	for _, exp := range expected {
+		if !strings.Contains(s, exp) {
+			fmt.Printf("Git repo in '%s' not clean.\nDidn't find '%s' in output of git status:\n%s\n", dir, exp, s)
+			os.Exit(1)
+		}
+	}
+}
+
+func readZipFile(path string) map[string][]byte {
+	r, err := zip.OpenReader(path)
+	must(err)
+	defer fileClose(r)
+	res := map[string][]byte{}
+	for _, f := range r.File {
+		rc, err := f.Open()
+		must(err)
+		d, err := ioutil.ReadAll(rc)
+		must(err)
+		_ = rc.Close()
+		res[f.Name] = d
+	}
+	return res
+}
+
+func zipAddFile(zw *zip.Writer, zipName string, path string) {
+	zipName = filepath.ToSlash(zipName)
+	d, err := ioutil.ReadFile(path)
+	must(err)
+	w, err := zw.Create(zipName)
+	_, err = w.Write(d)
+	must(err)
+	fmt.Printf("  added %s from %s\n", zipName, path)
+}
+
+func zipDirRecur(zw *zip.Writer, baseDir string, dirToZip string) {
+	dir := filepath.Join(baseDir, dirToZip)
+	files, err := ioutil.ReadDir(dir)
+	must(err)
+	for _, fi := range files {
+		if fi.IsDir() {
+			zipDirRecur(zw, baseDir, filepath.Join(dirToZip, fi.Name()))
+		} else if fi.Mode().IsRegular() {
+			zipName := filepath.Join(dirToZip, fi.Name())
+			path := filepath.Join(baseDir, zipName)
+			zipAddFile(zw, zipName, path)
+		} else {
+			path := filepath.Join(baseDir, fi.Name())
+			s := fmt.Sprintf("%s is not a dir or regular file", path)
+			panic(s)
+		}
+	}
+}
+
+func createZipFile(dst string, baseDir string, toZip ...string) {
+	removeFile(dst)
+	if len(toZip) == 0 {
+		panic("must provide toZip args")
+	}
+	fmt.Printf("Creating zip file %s\n", dst)
+	w, err := os.Create(dst)
+	must(err)
+	defer fileClose(w)
+	zw := zip.NewWriter(w)
+	must(err)
+	for _, name := range toZip {
+		path := filepath.Join(baseDir, name)
+		fi, err := os.Stat(path)
+		must(err)
+		if fi.IsDir() {
+			zipDirRecur(zw, baseDir, name)
+		} else if fi.Mode().IsRegular() {
+			zipAddFile(zw, name, path)
+		} else {
+			s := fmt.Sprintf("%s is not a dir or regular file", path)
+			panic(s)
+		}
+	}
+	err = zw.Close()
 	must(err)
 }
