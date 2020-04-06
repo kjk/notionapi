6630aca655c249dee784255598773aa5f41107f7
Rawipon Tubtimtong <rawipon.t@gmail.com>
2020-04-06 15:41:09 +0700

    add testing bypass page cover

--- tohtml/html_test.go
+++ tohtml/html_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/kjk/notionapi"
 )
 
 func TestHTMLFileNameForPage(t *testing.T) {
@@ -14,4 +15,13 @@ func TestHTMLFileNameForPage(t *testing.T) {
 		got := htmlFileName(test[0])
 		assert.Equal(t, test[1], got)
 	}
+}
+
+func TestByPassPageCover(t *testing.T) {
+	ByPassPageCover(true)
+
+	uri := "https://example.com/image.jpg"
+	res := FilePathFromPageCoverURL(uri, &notionapi.Block{})
+
+	assert.Equal(t, uri, res)
 }
