67e75173f1991ba68803231243d29aca5b584f78
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2021-10-11 04:17:05 +0000

    go back to using jsonit for js pretty printing

--- json.go
+++ json.go
@@ -7,24 +7,41 @@ import (
 	"github.com/tidwall/pretty"
 )
 
-var jsonit = jsoniter.ConfigCompatibleWithStandardLibrary
-
-var prettyOpts = pretty.Options{
-	Width:  80,
-	Prefix: "",
-	Indent: "  ",
-	// sorting keys only slightly slower
-	SortKeys: true,
-}
+var (
+	PrettyPrintJS = PrettyPrintJSJsonit
+	jsonit        = jsoniter.ConfigCompatibleWithStandardLibrary
+	prettyOpts    = pretty.Options{
+		Width:  80,
+		Prefix: "",
+		Indent: "  ",
+		// sorting keys only slightly slower
+		SortKeys: true,
+	}
+)
 
 // TODO: doesn't work with some of Notion json responses?
 // pretty-print if valid JSON. If not, return unchanged
 // about 4x faster than naive version using json.Unmarshal() + json.Marshal()
-func PrettyPrintJSBroken(js []byte) []byte {
+func PrettyPrintJSJsonit(js []byte) []byte {
 	if !jsonit.Valid(js) {
 		return js
 	}
 	return pretty.PrettyOptions(js, &prettyOpts)
+}
+
+// pretty-print if valid JSON. If not, return unchanged
+// about 4x faster than naive version using json.Unmarshal() + json.Marshal()
+func PrettyPrintJSStd(js []byte) []byte {
+	var m map[string]interface{}
+	err := json.Unmarshal(js, &m)
+	if err != nil {
+		return js
+	}
+	d, err := json.MarshalIndent(m, "", "  ")
+	if err != nil {
+		return js
+	}
+	return d
 }
 
 func jsonUnmarshalFromMap(m map[string]interface{}, v interface{}) error {
@@ -42,19 +59,4 @@ func jsonGetMap(m map[string]interface{}, key string) map[string]interface{} {
 		}
 	}
 	return nil
-}
-
-// pretty-print if valid JSON. If not, return unchanged
-// about 4x faster than naive version using json.Unmarshal() + json.Marshal()
-func PrettyPrintJS(js []byte) []byte {
-	var m map[string]interface{}
-	err := json.Unmarshal(js, &m)
-	if err != nil {
-		return js
-	}
-	d, err := json.MarshalIndent(m, "", "  ")
-	if err != nil {
-		return js
-	}
-	return d
 }
